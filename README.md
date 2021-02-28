2ac29be3fb7e39c045e57edb3201a8f8537ee76d
amathael <dan.io.oreshnikov@gmail.com>
2021-03-01 00:48:05 +0300

    Implemented gradient descent

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ## Файлы
 
-- [data/oracle](data/oracle.py) &ndash; обертка над функцией и ее градиентом
+- [utils](utils)
+    - [optimizer](utils/optimizer.py) &ndash; абстрактный класс для линейного оптимизатора
+    - [oracle](utils/oracle.py) &ndash; обертка над функцией и ее градиентом
 - [gradient](gradient.py) &ndash; функция градиентного спуска
--- gradient.py
+++ gradient.py
@@ -1,10 +1,28 @@
 from typing import Callable, List, Tuple, Union
-from data.oracle import Oracle
+
+import numpy as np
+
+from utils.oracle import Oracle
+from utils.optimizer import Optimizer
 
 
-def gradient(
-        f: Oracle, bounds: List[Tuple[Float, Float]],
-        learning_rate: Union[float, Callable[..., float]],
-        iterations: int = 1000, eps: float = None,
-):
-    pass
+def gradient_descent(
+        f: Oracle, x0: np.ndarray, step_optimizer: Callable[..., Optimizer],
+        iterations: int = 1000, eps: float = 1e-6,
+) -> np.ndarray:
+    x = x0
+    it = 0
+
+    while it < iterations:
+        grad = f.grad(*x)
+
+        def g(lmbd):
+            return f(*(x - grad * lmbd))
+
+        delta = grad * step_optimizer(g, (0, 2), eps).optimize()
+        x -= delta
+        if np.linalg.norm(delta) < eps:
+            break
+        it += 1
+
+    return x
