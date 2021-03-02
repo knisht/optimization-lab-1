48b2cd923b63339d8aa241ca0dfc7f96a00f497e
Mikhail Terentev <m.y.terentev@gmail.com>
2021-03-02 12:56:51 +0300

    FibOptimizer added

--- searchers/FibOptimizer.py
+++ searchers/FibOptimizer.py
@@ -11,16 +11,16 @@ class FibOptimizer(Optimizer):
         self.n = self.genF(a, b, window)
         self.k = 1
 
-        self.l = [0] * (self.n + 1)
+        self.l = [0.0] * (self.n + 1)
         self.l[1] = a + self.F[self.n - 2] / self.F[self.n] * (b - a)
 
-        self.mu = [0] * (self.n + 1)
+        self.mu = [0.0] * (self.n + 1)
         self.mu[1] = a + self.F[self.n - 1] / self.F[self.n] * (b - a)
 
-        self.a = [0] * (self.n + 1)
+        self.a = [0.0] * (self.n + 1)
         self.a[1] = a
 
-        self.b = [0] * (self.n + 1)
+        self.b = [0.0] * (self.n + 1)
         self.b[1] = b
 
     def _step(self, a: float, b: float) -> Tuple[float, float]:
@@ -34,7 +34,7 @@ class FibOptimizer(Optimizer):
             self.a[k + 1] = self.l[k]
             self.b[k + 1] = self.b[k]
             self.l[k + 1] = self.mu[k]
-            self.mu[k + 1] = a + self.F[n - k - 1] / self.F[n - k] * (self.b[k + 1] - self.a[k + 1])
+            self.mu[k + 1] = self.a[k + 1] + self.F[n - k - 1] / self.F[n - k] * (self.b[k + 1] - self.a[k + 1])
         else:
             self.a[k + 1] = self.a[k]
             self.b[k + 1] = self.mu[k]
@@ -46,15 +46,14 @@ class FibOptimizer(Optimizer):
             return self.a[k], self.b[k]
 
         self.l[self.n] = self.l[self.n - 1]
-        self.mu = self.l + self.eps
+        self.mu[self.n] = self.l[self.n] + self.eps
 
         if self.f(self.l[self.n]) == self.f(self.mu[n]):
             self.a[n] = self.mu[n]
             self.b[n] = self.b[n - 1]
         else:
-            if self.f(self.l[self.n]) == self.f(self.mu[n]):
-                self.a[n] = self.a[n - 1]
-                self.b[n] = self.mu[n]
+            self.a[n] = self.a[n - 1]
+            self.b[n] = self.mu[n]
 
         return self.a[k + 1], self.b[k + 1]
 
