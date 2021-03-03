d4920350c91fc71bc1ff5ce838bd91e2efdf9b52
Mikhail Terentev <m.y.terentev@gmail.com>
2021-03-03 11:39:54 +0300

    refactoring

--- optimize/methods/fibonacci.py
+++ optimize/methods/fibonacci.py
@@ -8,52 +8,49 @@ class FibonacciOptimizer(Optimizer):
         super().__init__(f, bounds, eps)
         self.F = []
         self.n = self.genF(a, b, window)
-        self.k = 0
 
-        self.l = [0.0] * (self.n + 1)
+        self.l = [0.0] * self.n
         self.l[0] = a + self.F[self.n - 2] / self.F[self.n] * (b - a)
 
-        self.mu = [0.0] * (self.n + 1)
+        self.mu = [0.0] * self.n
         self.mu[0] = a + self.F[self.n - 1] / self.F[self.n] * (b - a)
 
-        self.a = [0.0] * (self.n + 1)
+        self.a = [0.0] * self.n
         self.a[0] = a
 
-        self.b = [0.0] * (self.n + 1)
+        self.b = [0.0] * self.n
         self.b[0] = b
 
-    # TODO я поменял сигнатуру _step и optimize, предлагаю тут вообще _step выкинуть и просто переопределить optimize
-    def _step(self, a: float, b: float) -> Tuple[float, float]:
+    def optimize(self) -> float:
+        self.history = []
         n = self.n
-        k = self.k
-        if self.f(self.l[k]) > self.f(self.mu[k]):
-            self.a[k + 1] = self.l[k]
-            self.b[k + 1] = self.b[k]
-            self.l[k + 1] = self.mu[k]
-            self.mu[k + 1] = self.a[k + 1] + self.F[n - k - 1] / self.F[n - k] * (self.b[k + 1] - self.a[k + 1])
-        else:
-            self.a[k + 1] = self.a[k]
-            self.b[k + 1] = self.mu[k]
-            self.mu[k + 1] = self.l[k]
-            self.l[k + 1] = self.a[k + 1] + self.F[n - k - 2] / self.F[n - k] * (self.b[k + 1] - self.a[k + 1])
+        for k in range(self.n):
+            self._log(self.a[k], self.b[k])
+            if self.f(self.l[k]) > self.f(self.mu[k]):
+                self.a[k + 1] = self.l[k]
+                self.b[k + 1] = self.b[k]
+                self.l[k + 1] = self.mu[k]
+                self.mu[k + 1] = self.a[k + 1] + self.F[n - k - 1] / self.F[n - k] * (self.b[k + 1] - self.a[k + 1])
+            else:
+                self.a[k + 1] = self.a[k]
+                self.b[k + 1] = self.mu[k]
+                self.mu[k + 1] = self.l[k]
+                self.l[k + 1] = self.a[k + 1] + self.F[n - k - 2] / self.F[n - k] * (self.b[k + 1] - self.a[k + 1])
 
-        if self.k != self.n - 2:
-            self.k = k + 1
-            # TODO я добавил метод _log(a, b), надо вызывать его каждый раз, когда выбираешь новые границы
-            # в начале optimize надо будет делать self.history = []
-            return self.a[k], self.b[k]
+            if k != self.n - 2:
+                continue
 
-        self.l[self.n] = self.l[self.n - 1]
-        self.mu[self.n] = self.l[self.n] + self.eps
+            self.l[self.n] = self.l[self.n - 1]
+            self.mu[self.n] = self.l[self.n] + self.eps
 
-        if self.f(self.l[self.n]) == self.f(self.mu[n]):
-            self.a[n] = self.mu[n]
-            self.b[n] = self.b[n - 1]
-        else:
-            self.a[n] = self.a[n - 1]
-            self.b[n] = self.mu[n]
+            if self.f(self.l[self.n]) == self.f(self.mu[n]):
+                self.a[n] = self.mu[n]
+                self.b[n] = self.b[n - 1]
+            else:
+                self.a[n] = self.a[n - 1]
+                self.b[n] = self.mu[n]
 
-        return self.a[k + 1], self.b[k + 1]
+        return 0.0
 
     def genF(self, a, b, l):
         min = (b - a) / l
