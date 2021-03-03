86a7050454b6728457a40fb7daea573006af0a01
Mikhail Terentev <m.y.terentev@gmail.com>
2021-03-03 13:02:00 +0300

    lst added

--- optimize/methods/fibonacci.py
+++ optimize/methods/fibonacci.py
@@ -13,7 +13,6 @@ class FibonacciOptimizer(Optimizer):
 
         self.n = self.genF(a0, b0, eps)
 
-
         self.l = [0.0] * self.n
         self.l[0] = a0 + self.F[self.n - 2] / self.F[self.n - 1] * (b0 - a0)
 
@@ -28,35 +27,37 @@ class FibonacciOptimizer(Optimizer):
 
     def optimize(self) -> float:
         self.history = []
-        n = self.n
+        lst = self.n - 1
         for k in range(self.n):
             self._log(self.a[k], self.b[k])
             if self.f(self.l[k]) > self.f(self.mu[k]):
                 self.a[k + 1] = self.l[k]
                 self.b[k + 1] = self.b[k]
                 self.l[k + 1] = self.mu[k]
-                self.mu[k + 1] = self.a[k + 1] + self.F[n - k - 1] / self.F[n - 1 - k] * (self.b[k + 1] - self.a[k + 1])
+                self.mu[k + 1] = self.a[k + 1] + self.F[lst - k - 1] / self.F[lst - k] * (
+                            self.b[k + 1] - self.a[k + 1])
             else:
                 self.a[k + 1] = self.a[k]
                 self.b[k + 1] = self.mu[k]
                 self.mu[k + 1] = self.l[k]
-                self.l[k + 1] = self.a[k + 1] + self.F[n - k - 2] / self.F[n - 1 - k] * (self.b[k + 1] - self.a[k + 1])
+                self.l[k + 1] = self.a[k + 1] + self.F[lst - k - 2] / self.F[lst - k] * (
+                            self.b[k + 1] - self.a[k + 1])
 
-            if k != self.n - 3:
+            if k != lst - 2:
                 continue
 
             self.l[self.n] = self.l[self.n - 1]
             self.mu[self.n] = self.l[self.n - 1] + self.eps
 
-            if self.f(self.l[self.n - 1]) == self.f(self.mu[n - 1]):
-                self.a[n - 1] = self.mu[n - 1]
-                self.b[n - 1] = self.b[n - 1]
+            if self.f(self.l[lst]) == self.f(self.mu[lst]):
+                self.a[lst] = self.mu[lst]
+                self.b[lst] = self.b[lst]
             else:
-                self.a[n - 1] = self.a[n - 1]
-                self.b[n - 1] = self.mu[n - 1]
+                self.a[lst] = self.a[lst]
+                self.b[lst] = self.mu[lst]
 
-        self._log(self.a[n - 1], self.b[n - 1])
-        return (self.a[n - 1] + self.b[n - 1]) / 2
+        self._log(self.a[lst], self.b[lst])
+        return (self.a[lst] + self.b[lst]) / 2
 
     def genF(self, a, b, l):
         min = (b - a) / l
