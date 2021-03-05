f37794fea4844ac35e8bf5019b9552d677b39c00
Mikhail Terentev <m.y.terentev@gmail.com>
2021-03-05 18:48:54 +0300

    fixes

--- optimize/methods/fibonacci.py
+++ optimize/methods/fibonacci.py
@@ -17,24 +17,23 @@ class FibonacciOptimizer(Optimizer):
         self.history = []
         a = self.bounds[0]
         b = self.bounds[1]
-        self._log(a, b)
         x1 = a + self.F[self.n] / self.F[self.n + 2] * (b - a)
         x2 = a + self.F[self.n + 1] / self.F[self.n + 2] * (b - a)
         f1, f2 = self.f(x1), self.f(x2)
         self.f_calls = 2
-        for i in range(1, self.n + 1):
+        for i in range(1, self.n):
             if f2 < f1:
+                f1 = f2
                 a = x1
                 x1 = x2
                 x2 = a + self.F[self.n + 1 - i] / self.F[self.n + 2 - i] * (b - a)
-                f1 = f2
                 f2 = self.f(x2)
 
             else:
+                f2 = f1
                 b = x2
                 x2 = x1
                 x1 = a + self.F[self.n - i] / self.F[self.n + 2 - i] * (b - a)
-                f2 = f1
                 f1 = self.f(x1)
             self.f_calls += 1
             self._log(a, b)
@@ -42,7 +41,8 @@ class FibonacciOptimizer(Optimizer):
 
     def genF(self, a, b, l):
         min = (b - a) / l
-        self.F = [1, 1]
+        self.F = [1, 1,]
+        n = 2
         while self.F[-1] < min:
             self.F.append(self.F[-1] + self.F[-2])
         n = len(self.F)
